f80ad3b10096c847f8a58d7fd7b047ef97d9a92c
craigmcginley <craigmcginley@gmail.com>
2018-09-07 16:23:32 -0400

    fix homepage featured text padding

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,32 +23,32 @@ const IndexPage = () => (
     <div className="max-width content-padding">
       <div className="pure-g margin-vertical--double">
         <div className="pure-u-1 pure-u-lg-1-3">
-          <h2 className="header--xl margin--none">Our designs in healthcare are used every day<span className="text--serif text--primary">.</span></h2>
+          <h2 className="header--xl margin--none pad-right--double">Our designs in healthcare are used every day<span className="text--serif text--primary">.</span></h2>
         </div>
         <div className="pure-u-1 pure-u-lg-2-3">
           <div className="pure-g">
             <div className="pure-u-1 pure-u-lg-1-2">
-              <p>
+              <p className="pad-right--double">
                 <span className="text--bold">160 million US residents</span><br/>
                 <span className="text--gray">are analyzed, care planned, and risk adjusted with the software we design.</span><br/>
               </p>
             </div>
             <div className="pure-u-1 pure-u-lg-1-2">
-              <p>
+              <p className="pad-right--double">
                 <span className="text--bold">700,000 Massachusetts residents</span><br/>
                 <span className="text--gray">count on our service design.</span><br/>
                 <Link to="#">Read the case study</Link>
               </p>
             </div>
             <div className="pure-u-1 pure-u-lg-1-2">
-              <p>
+              <p className="pad-right--double">
                 <span className="text--bold">Wikipedia</span><br/>
                 <span className="text--gray">relies on our data visualization to explain complex health topics.</span><br/>
                 <Link to="#">Social determinants of health</Link>
               </p>
             </div>
             <div className="pure-u-1 pure-u-lg-1-2">
-              <p>
+              <p className="pad-right--double">
                 <span className="text--bold">1 billion prescriptions</span><br/>
                 <span className="text--gray">will flow through the software we've designed in 2019.</span><br/>
               </p>

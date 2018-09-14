d24efcc8df02f8a934dd08a5698732e7a348f37e
craigmcginley <craigmcginley@gmail.com>
2018-09-14 15:49:59 -0400

    add soundcloud embed

--- src/pages/vision.js
+++ src/pages/vision.js
@@ -135,6 +135,15 @@ const VisionPage = () => (
               title="The Digital Life"
               image="features/determinants-of-health/feature_banner.jpg"
               caption="Explore our podcast on emerging technology and digital design." />
+            <iframe
+              title="GoInvo SoundCloud Embed"
+              width="100%"
+              height="450"
+              scrolling="no"
+              frameborder="no"
+              allow="autoplay"
+              src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/14454933&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true">
+            </iframe>
             <a className="float--right" href="https://soundcloud.com/involution-studios" target="_blank" rel="noopener noreferrer">View all episodes on SoundCloud</a>
           </div>
         </div>

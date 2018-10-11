7cd556a15bd374a2fadac09f3926e376bf7da0ff
Jennifer <jennifer@goinvo.com>
2018-10-11 08:51:27 -0400

    determinants of health feature

---
+++ src/pages/vision/determinants-of-health.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Layout from '../components/layouts/layout'
+import Hero from '../components/hero'
+
+const DeterminantsOfHealthFeature = () => (
+  <Layout>
+    <Hero image="/images/contact/studio.jpg"></Hero>
+    <div className="background--blue pad-vertical--double">
+      <div className="max-width content-padding">
+
+
+      </div>
+    </div>
+  </Layout>
+)
+
+export default DeterminantsOfHealthFeature

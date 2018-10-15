5856194c23e4c19ec5abfcf821b96e652cc96b99
Jennifer <jennifer@goinvo.com>
2018-10-15 17:36:24 -0400

    doh chart selection enabled

---
+++ src/pages/vision/determinants-of-health/determinant-details.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+
+class DeterminantDetails extends Component {
+  render() {
+    const { title, description, divisions } = this.props;
+
+    return (
+      <div>
+        <h3 className="determinant">{title}</h3>
+        <p className="description">{description}</p>
+        <div className="divisions">
+          {divisions.map(division => {
+            return (
+              <div className="divisionSection" key={division.title}>
+                <h4 className="division">{division.title}</h4>
+                {
+                  division.factors ?
+                    <ul className="factors">
+                      {division.factors.map(factor => {
+                        return (
+                          <li key={factor} className="factor">{factor}</li>
+                        )
+                      })}
+                    </ul>
+                  : null
+                }
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default DeterminantDetails
--- src/pages/vision/determinants-of-health/visualization.js
+++ src/pages/vision/determinants-of-health/visualization.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import Chart from 'react-google-charts'
 
-import determinants from './visualization-data.json'
-
+/*TODO: use json to populate */
 const data = [
   ["Determinant", "Percentage"],
   ["Individual Behavior", 36],
@@ -23,10 +22,12 @@ const options = {
   tooltip: { trigger: "none" },
   colors: ["#F9D7A7", "#B2E5E9", "#E8ED9D", "#F8CBC5", "#90EED4"]
 };
-const { title, description, division } = this.props.determinant;
-const { title, factor } = this.props.division;
 
 class Chartdoh extends React.Component {
+  handleSelect = ({ chartWrapper }) => {
+    this.props.onSelect(chartWrapper.getChart().getSelection());
+  }
+
   render() {
     return (
         <Chart
@@ -35,132 +36,15 @@ class Chartdoh extends React.Component {
           height="400px"
           data={data}
           options={options}
+          chartEvents={[
+            {
+              eventName: "select",
+              callback: this.handleSelect
+            }
+          ]}
         />
-
-          //for selected determinant
-          <ChartDetails>
-            <h4> {determinant.title} </h4>
-            <p> {determinant.descriptions} </p>
-            <div className="divisions">
-
-                //loop through all divisions
-                <div className="factors">
-                  //loop through all factors
-                  <h5>{determinant.division.title}</h5>
-                  <ul>
-                    <li>{determinant.division.factor}</li>
-                  </ul>
-                </div>
-            </div>
-          />
     );
   }
 }
 
 export default Chartdoh
-
-google.visualization.events.addListener(chart, 'select', selectHandler);
-selectHandlerApplied = true;
-
-function selectHandler(e) {
-  var selectedItem = chart.getSelection()[0];
-  // console.log(selectedItem);
-
-  if (selectedItem) {
-    currentSelection = selectedItem.row;
-    $("#determinant-info").html(template({determinantIndex: currentSelection, data: chartData}));
-  }
-}
-google.visualization.events.addListener(chart, 'ready', function() {
-    // set the selection to the first row in the chart
-    chart.setSelection([{"row": 3}]);
-    $("#determinant-info").html(template({determinantIndex: 3, data: chartData}));
-});
-
-/*$(document).ready(function() {
-  google.charts.load("current", {packages:["corechart"]});
-  google.charts.setOnLoadCallback(getChartData);
-
-  var $chartContainer = $('#determinants-chart');
-  var chartData = [];
-  var currentSelection = 0;
-
-  function formatData(data) {
-    // Grab the keys for the google charts data column titles
-    var keys = Object.keys(data[0]);
-    // Add the column titles as the first row
-    var chartDeterminants = [keys];
-    // Add rows for each determinant
-    _.each(data, function(determinant, i) {
-      chartDeterminants.push([]);
-      _.each(keys, function(key) {
-        chartDeterminants[i + 1].push(determinant[key]);
-      });
-    });
-    return google.visualization.arrayToDataTable(chartDeterminants);
-  }
-
-  function getChartData() {
-    $.getJSON("/features/determinants-of-health/visualization-data.json", function( data ) {
-      chartData = formatData(data);
-
-      drawChart();
-    });
-  }
-  */
-
-  /*function drawChart() {
-
-    // Resize the container div
-    var chartdiv = $("#determinants-chart");
-    var factor = Math.max(Math.min(.55, (chartdiv.width()-600)/800), .4);
-    chartdiv.height(chartdiv.width()*factor + "px");
-
-    var options = {
-      chartArea: { left: 10, top: 10, width: '90%', height: '90%' },
-      legend: { position: "labeled" },
-      pieHole: 0.25,
-      pieSliceText: 'none',
-      pieSliceTextStyle: { 'color': '#444', 'font-size': '12px' },
-      fontName: "Open Sans",
-      fontColor: "#444",
-      tooltip: {"trigger":"none"},
-      colors: ['#F9D7A7', '#B2E5E9', '#E8ED9D', '#F8CBC5', '#90EED4']
-    };
-
-    var chart = new google.visualization.PieChart($chartContainer[0]);
-    chart.draw(chartData, options);
-
-    var template = $("#determinant-info-template").html();
-    var template = _.template(template);
-
-    $("#determinant-info").html(template({determinantIndex: currentSelection, data: chartData}));
-
-    google.visualization.events.addListener(chart, 'select', selectHandler);
-    selectHandlerApplied = true;
-
-    function selectHandler(e) {
-      var selectedItem = chart.getSelection()[0];
-      // console.log(selectedItem);
-
-      if (selectedItem) {
-        currentSelection = selectedItem.row;
-        $("#determinant-info").html(template({determinantIndex: currentSelection, data: chartData}));
-      }
-    }
-
-
-    google.visualization.events.addListener(chart, 'ready', function() {
-        // set the selection to the first row in the chart
-        chart.setSelection([{"row": 3}]);
-        $("#determinant-info").html(template({determinantIndex: 3, data: chartData}));
-    });
-
-  }
-
-  $(window).resize(function(){
-    drawChart();
-  });
-});
-
-*/

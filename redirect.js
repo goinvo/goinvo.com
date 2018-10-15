7bcd72ceb2a386153a88dee3b0cf8773cefc7ab3
craigmcginley <craigmcginley@gmail.com>
2018-10-15 15:43:02 -0400

    updates to redirects

--- redirect.js
+++ redirect.js
@@ -1,8 +1,8 @@
 var AWS = require('aws-sdk');
-AWS.config.update(config);
+AWS.config.update({ region: 'us-east-1' });
 
 var s3 = new AWS.S3({
-	// params: {Bucket: 'www.goinvo.com'}
+	params: {Bucket: 'www.goinvo.com-2018'}
 });
 
 var jsonObj = require("./redirects.json");
@@ -10,12 +10,12 @@ var jsonObj = require("./redirects.json");
 for(var myRedirect in jsonObj) {
 	s3.putObject ({
 		Key: myRedirect,
-		WebsiteRedirectLocation: 'http://'+jsonObj[myRedirect]
+		WebsiteRedirectLocation: jsonObj[myRedirect]
 	}, (err,data) => {
     if(err) {
-        console.log(err)
+				console.log("Error:", err)
     } else {
-    	console.log(data)
+    	console.log("Success:", data)
     }
-  }));
+  });
 }

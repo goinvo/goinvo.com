298d7f1ce4d66506e63ff6f3a32bb7d53da98d62
Jennifer <jennifer@goinvo.com>
2018-10-23 12:57:54 -0400

    feature oshc styles

--- src/pages/vision/open-source-healthcare/index.js
+++ src/pages/vision/open-source-healthcare/index.js
@@ -11,36 +11,36 @@ class OpenSourceHealthcareFeature extends Component {
   render() {
     return (
       <Layout>
-        <Hero image="/images/services/doh-preview.jpg"></Hero>
+        <Hero image="/images/features/open-source-healthcare/open-source-healthcare-hero.jpg"></Hero>
         <div className="oshc-feature">
           <div className="pad-vertical--double">
             <div className="max-width max-width--md content-padding">
-              <div className="title-area">
+              <div className="oshc-title-area">
                 <h1 className="header--xl">Open Source Healthcare</h1>
-                <h3 className="header--md">We Must Set Healthcare Free</h3>
-                <p className="text--grey">Open source is something that anyone can change and share, because it’s publicly available under a generous license. While it first began with computer code, open source now influences how projects and businesses work, and our lives benefit from this open sharing. Open source has grown into a way of participating with many others that asks for transparency, community-based collaboration, and meritocracy. The best ideas float to the top, and you earn trust by what you do and how you amplify the group.</p>
-                <p className="text--grey">Our internet is infused with open source ideas and services &mdash; from how cell phones communicate, to how e-mail is directed from one person to the next, to Linux. All of these technologies working together are the operating system of the internet.</p>
-                <p className="text--grey">Here in the US, healthcare is
+                <h3 className="header--md text--primary">We Must Set Healthcare Free</h3>
+                <p className="text--gray">Open source is something that anyone can change and share, because it’s publicly available under a generous license. While it first began with computer code, open source now influences how projects and businesses work, and our lives benefit from this open sharing. Open source has grown into a way of participating with many others that asks for transparency, community-based collaboration, and meritocracy. The best ideas float to the top, and you earn trust by what you do and how you amplify the group.</p>
+                <p className="text--gray">Our internet is infused with open source ideas and services &mdash;from how cell phones communicate, to how e-mail is directed from one person to the next, to Linux. All of these technologies working together are the operating system of the internet.</p>
+                <p className="text--gray">Here in the US, healthcare is
                   <br />sometimes amazing,
                   <br />often lifesaving,
                   <br />always expensive,
                   <br />and mostly closed.</p>
-                <p className="text--grey">It’s tribal at its core &mdash; each hospital, each doc, each healthcare system invents its own way &mdash; to the detriment of our collective health.</p>
+                <p className="text--gray">It’s tribal at its core &mdash;each hospital, each doc, each healthcare system invents its own way &mdash;to the detriment of our collective health.</p>
                 <Divider />
               </div>
 
-              <div className="introduction">
-                <h4>We have open standards for finance</h4>
-                <p className="text--gray">because we value our money more than our health.</p>
+              <div className="oshc-introduction">
+                <h4 className="header--sm margin-bottom--none">We have open standards for finance</h4>
+                <p className="text--gray margin-top--none margin-bottom--double">because we value our money more than our health.</p>
 
-                <h4>We have open standards for transportation</h4>
-                <p className="text--gray">because getting to your destination is a necessity.</p>
+                <h4 className="header--sm margin-bottom--none">We have open standards for transportation</h4>
+                <p className="text--gray margin-top--none margin-bottom--double">because getting to your destination is a necessity.</p>
 
-                <h4>We have open standards for healthcare</h4>
-                <p className="text--gray">because our lives depend on it.</p>
+                <h4 className="header--sm margin-bottom--none">We have open standards for healthcare</h4>
+                <p className="text--gray margin-top--none margin-bottom--double">because our lives depend on it.</p>
 
-                <p>Read our open source ethos,
-                  <br /> with contributing articles by <a href="">Jane Sarasohn-Kahn</a> and <a href="">Eric Topol</a></p>
+                <p className="text--gray">Read our open source ethos,
+                  <br /> with contributing articles by <a href="https://www.healthpopuli.com/2018/10/11/open-source-health-care-will-liberate-patients/" target="_blank" rel="noopener noreferrer">Jane Sarasohn-Kahn</a> and <a href="https://twitter.com/EricTopol" target="_blank" rel="noopener noreferrer">Eric Topol</a></p>
 
                   <div className="button-group margin-bottom--double">
                     <div className="pure-u-1 pure-u-lg-1-2"><a href="" target="_blank" rel="noopener noreferrer" className="button button--primary button--lg margin-top--double margin-bottom--double button--block">Download 25 MB PDF</a></div>
@@ -49,15 +49,14 @@ class OpenSourceHealthcareFeature extends Component {
                 <Divider />
               </div>
 
-              <div cassName="missionette">
-                <h2>Open Source Healthcare Missionette</h2>
-                <p>We live in a closed healthcare system.
+              <div cassName="oshc-missionette">
+                <h2 className="header--lg margin-top--double">Open Source Healthcare Missionette</h2>
+                <p className="text--gray">We live in a closed healthcare system.
                     <br />The algorithms that drive our care,
                     <br />to our clinical and life data,
                     <br />to hospital and treatment pricing,
                     <br />are governed by blackbox services.</p>
-
-                <p>By using these closed systems,
+                <p className="text--gray">By using these closed systems,
                   <br />we are actively designed out
                   <br />of the decision-making process,
                   <br />in favor of corporate “optimized care”
@@ -65,11 +64,9 @@ class OpenSourceHealthcareFeature extends Component {
                   <br />The crooked biases built into software,
                   <br />implemented with intent or accidentally,
                   <br />need interrogation, citizen collaboration, and correction.</p>
-
-                <p>It’s our health.
+                <p className="text--gray">It’s our health.
                   <br />Our very lives are at stake.</p>
-
-                <p>We demand that our healthcare services be open
+                <p className="text--gray">We demand that our healthcare services be open
                   <br />to inspect and correct bias,
                   <br />to be accessible for rapid innovation and evolution,
                   <br />and to become more valuable as

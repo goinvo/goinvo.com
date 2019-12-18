f05482b71024e8b939544bbb26d67d75e3a26645
craigmcginley <craigmcginley@gmail.com>
2019-12-18 13:41:05 -0500

    add image preview for posters (poster cards).. still need high res images

--- src/components/vision/health-visualizations/poster-card.js
+++ src/components/vision/health-visualizations/poster-card.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react'
+import { Lightbox } from 'react-modal-image'
+
 //import Image from '../../image'
 import BackgroundImage from '../../background-image'
 import { mediaUrl } from '../../../helpers'
 
 class PosterCard extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      previewOpen: false,
+    }
+  }
+
+  openPreview = () => {
+    this.setState({ previewOpen: true })
+  }
+
+  closePreview = () => {
+    this.setState({ previewOpen: false })
+  }
+
   render() {
     const { sizes, id, title, image, learnMoreLink = null } = this.props
 
@@ -13,19 +31,17 @@ class PosterCard extends Component {
 
     return (
       <div className="poster-card margin-top--double">
-        <div className="poster-preview">
-          <a href={downloadLink}>
-            <div className="image-block image-block--hoverable margin-top--none">
-              <div className="image-block__image-container">
-                <BackgroundImage
-                  src={image}
-                  sizes={sizes}
-                  className="image-block__image"
-                  alt={id}
-                />
-              </div>
+        <div className="poster-preview" onClick={this.openPreview}>
+          <div className="image-block image-block--hoverable margin-top--none">
+            <div className="image-block__image-container">
+              <BackgroundImage
+                src={image}
+                sizes={sizes}
+                className="image-block__image"
+                alt={id}
+              />
             </div>
-          </a>
+          </div>
         </div>
         <div className="posterInfo">
           <h4 className="header--sm margin-top--none">{title}</h4>
@@ -39,6 +55,13 @@ class PosterCard extends Component {
             {learnMoreLink ? <a href={learnMoreLink}>Learn More</a> : null}
           </p>
         </div>
+        {this.state.previewOpen && (
+          <Lightbox
+            large={mediaUrl(image)}
+            alt={title}
+            onClose={this.closePreview}
+          />
+        )}
       </div>
     )
   }

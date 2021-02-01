ebc87332e0667a911cbaf0f511f3ab4d21507604
Eric Benoit <benwa02@gmail.com>
2021-02-01 17:03:25 -0500

    Updating feature content.

--- src/pages/vision/us-healthcare-problems/index.js
+++ src/pages/vision/us-healthcare-problems/index.js
@@ -27,13 +27,29 @@ class USHealthcareProblemsFeature extends Component {
           <div className="pad-vertical--double">
             <div className="max-width max-width--md content-padding">
               <h1 className="header--xl">US Healthcare Problems</h1>
-              <h4 className="header--sm">
+              <p>
                 The United States is home to some of the best and worst care.
-                The list serves as a call to action for everyone to close the
-                gap.
-              </h4>
-              <p className="text--grey">
-                We can write a lovely paragraph here... or two.
+                The US Healthcare Problems list is the product of GoInvo’s
+                efforts to understand why these gaps exist and how they impact
+                us. By drawing attention to the most prominent problems faced in
+                our healthcare system, we can start to envision how it may be
+                redesigned.
+              </p>
+              <p>
+                GoInvo has spent the last two months researching and breaking
+                down these problems. The result is a list ranked based on
+                several quantitative indicators, such as number of deaths,
+                number of American people impacted, and dollars spent or lost.
+                The interconnected nature of the US healthcare system makes it
+                nearly impossible to fully separate these problems. GoInvo has
+                coupled analytical skills and interest in health policy to bring
+                to light this complex web of menacing realities. Unsurprisingly,
+                all problems can be attributed to the fee-for-service system in
+                some way.
+              </p>
+              <p>
+                GoInvo encourages you to be appalled by these statistics... and
+                then do something about it.
               </p>
             </div>
 
@@ -66,6 +82,21 @@ class USHealthcareProblemsFeature extends Component {
             ></iframe>
           </div>
 
+          <div className="pad-vertical--double">
+            <div className="max-width max-width--md content-padding">
+              <h2 className="header--lg text--center margin-bottom--half margin-top--double">
+                We'd Like Your Feedback
+              </h2>
+              <p className="text--gray">
+                We’ve highlighted the top problems related to healthcare in the
+                US. Where data is available, we’ve also listed quantitative
+                metrics supporting the severity of each problem. Explore the
+                list and references then send your feedback on this draft to{' '}
+                <a href="mailto:hello@goinvo.com"> hello@goinvo.com</a>.
+              </p>
+            </div>
+          </div>
+
           <div className="background--gray pad-vertical--double">
             <div className="max-width max-width--md content-padding">
               <HubspotForm
@@ -81,11 +112,29 @@ class USHealthcareProblemsFeature extends Component {
               <div id="methodology">
                 <h2 className="header--xl text--center">Methodology</h2>
                 <p className="text--gray">
-                  Below is a description of the methodology used in creating the
-                  US Healthcare Problems list. It is also a record of versioned
-                  updates to the methodology based on continuing research and
-                  feedback. Thank you to those who have reached out and helped
-                  identify areas to improve.
+                  The US Healthcare Problems list began as a google spreadsheet
+                  research effort, with sources including “The Long Fix” by
+                  Vivan Lee, CDC, and CMS. Our team aimed to pool relevant
+                  quantitative and anecdotal information for each outlined
+                  problem, making it possible to start to prioritize what needs
+                  addressing. Ideally, the move away from a fee-for-service
+                  system and towards a value-based approach would alleviate some
+                  of this burden.
+                </p>
+                <p>
+                  Fee-for-service: payment is dependent on the quantity of care
+                </p>
+                <p>Value-based: payment is dependent on the quality of care</p>
+                <p>
+                  The initial ranking is based on the quantitative data as well
+                  as the prevalence of the problem as explained in research. The
+                  next iteration will implement a more robust algorithm that
+                  considers all of these factors using relative weightings.
+                </p>
+                <p>
+                  Stay tuned for V02 where we will showcase the top 50 problems,
+                  include a method of visually connecting related problems, and
+                  showcase the current status of a problem being addressed.
                 </p>
                 <h3 className="header--md margin-bottom--half margin-top--double">
                   v1 - 21.Jan.2021

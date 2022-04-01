c4a60d73590eafd08ff389b635ce6f5e8b084243
Craig McGinley <craigmcginley@gmail.com>
2022-04-01 13:30:00 -0400

    remove gatsby-plugin-manifest, not needed and fixes arm chip issue until all packages can be uprgaded

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,18 +4,18 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'GoInvo.com',
-        short_name: 'GoInvo',
-        start_url: '/',
-        background_color: '#fff',
-        theme_color: '#4a5e88',
-        display: 'minimal-ui',
-        icon: 'src/assets/images/favicon.png', // This path is relative to the root of the site.
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: 'GoInvo.com',
+    //     short_name: 'GoInvo',
+    //     start_url: '/',
+    //     background_color: '#fff',
+    //     theme_color: '#4a5e88',
+    //     display: 'minimal-ui',
+    //     icon: 'src/assets/images/favicon.png', // This path is relative to the root of the site.
+    //   },
+    // },
     'gatsby-plugin-sass',
     {
       resolve: `gatsby-mdx`,

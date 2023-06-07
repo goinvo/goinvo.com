f09fec55dfde825884589917ed4f9a5991d339f2
Craig McGinley <craigmcginley@gmail.com>
2023-06-07 16:25:01 -0400

    trigger build functions on netlify now that codeship isnt free

--- netlify/functions/s3-redirects.js
+++ netlify/functions/s3-redirects.js
@@ -1,11 +1,12 @@
 var AWS = require('aws-sdk')
+const path = require('path')
 AWS.config.update({ region: 'us-east-1' })
 
 var s3 = new AWS.S3({
   params: { Bucket: 'www.goinvo.com-2018' },
 })
 
-var jsonObj = require('./redirects.json')
+var jsonObj = require(path.join(__dirname, '..', '..', '/redirects.json'))
 
 for (var myRedirect in jsonObj) {
   s3.putObject(

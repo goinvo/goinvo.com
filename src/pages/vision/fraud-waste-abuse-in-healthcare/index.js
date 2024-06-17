db349973bd62c81567c9b5b274ab169851f44f13
Michelle Bourdon <michelle@bourdons.org>
2024-06-17 14:04:13 -0400

    Added CFWE Section

--- src/pages/vision/fraud-waste-abuse-in-healthcare/index.js
+++ src/pages/vision/fraud-waste-abuse-in-healthcare/index.js
@@ -60,14 +60,12 @@ class FraudWasteAbuseHealthcare extends Component {
                 and have distilled each into a single sentence.
               </p>
               <div className="columns">
-                <div className="columns__item columns__item--2">
-                  <p>
-                    Fraud: intentional misuse of healthcare system resources.<br />
-                    Waste: unintentional misuse of healthcare system resources.<br />
-                    Abuse: misuse of healthcare system resources independent of intention.<br />
-                  </p>
+                <div className="columns__item--2">
+                  <p><b>Fraud:</b> intentional misuse of healthcare system resources.</p>
+                  <p><b>Waste:</b> unintentional misuse of healthcare system resources.</p>
+                  <p><b>Abuse:</b> misuse of healthcare system resources independent of intention.</p>
                 </div>
-                <div className="columns__item columns__item--2">
+                <div className="columns__item--2">
                   <p>
                         Add later
                   </p>
@@ -97,6 +95,7 @@ class FraudWasteAbuseHealthcare extends Component {
                 <sup>
                   <a href="#references">11</a>
                 </sup>{' '}
+                <span className="text--primary">HHS</span>
                 <br />
               </p>
               <div className="margin-auto">
@@ -113,6 +112,7 @@ class FraudWasteAbuseHealthcare extends Component {
                 <sup>
                     <a href="#references">11</a>
                 </sup>{' '}
+                <span className="text--primary">FBI</span>
                 <br />
               </p>
               <div className="margin-auto">
@@ -149,7 +149,7 @@ class FraudWasteAbuseHealthcare extends Component {
               <Divider />
               <div className="pad-top--half pad-bottom--half pad-horizontal margin-bottom--double">
                 <h1 className="header--xl text--center max-width--sm margin-auto">
-                  Common Fraud, Waste, Abuse Enumeration (CFWAE)
+                  Common Fraud, Waste, Abuse Enumeration
                 </h1>
               </div>
               <p>
@@ -161,7 +161,7 @@ class FraudWasteAbuseHealthcare extends Component {
               FWA reporting tool. 
               </p>
               <h2 className="header--lg text--left">Common Fraud, Waste, Abuse Enumeration (CFWAE)</h2>
-              <p className="text--center"> 
+              <p className="text--left"> 
                 1.000 No service was provided but a charge was submitted      <br />
                 2.000 A service was provided but it was not necessary         <br />
                 3.000 A service was provided but a kickback was involved      <br />
@@ -170,46 +170,167 @@ class FraudWasteAbuseHealthcare extends Component {
                 6.000 A service was provided but the payment was denied       <br />
                 7.000 A service was provided but it was not covered           <br />
               </p>
-              <h2 className="header--lg text--left">1.000 No service was provided but a charge was submitted</h2>
+              <br />
+              <h2 className="header--md text--left">1.000 No service was provided but a charge was submitted</h2>
               <div className="background--gray pad-all text--left">
-                <p>
-                  Info
+                <p className="text--sm">
+                  Medicaid enrollment → Patient onboarding → Diagnosis / treatment → <b>Claims</b> → Post-treatment
+                </p>
+                <div className="columns">
+                  <div className="columns__item--2 container--align-center">
+                    <div className="pad-top text--center text--serif text--xl">19%</div>
+                    <p className="text--center">Of Fraud Cases</p>
+                  </div> 
+                  <div className="columns__item--2 container--align-center">
+                    <div className="pad-top text--center text--serif text--xl">$97M - $323M</div>
+                    <p className="text--center">Of US Medicaid</p>
+                  </div>  
+                </div>
+                <p className="text--sm">
+                  Examples <br />
+                  A provider bills Medicaid for an x-ray that was never taken for the patient.<br />
+                  A patient failed to keep his appointment but a charge was submitted as if the appointment took place.<br />
+                  Patient was billed for 30 tablets of Oxycontin but half of this amount was delivered to the patient.<br />
                 </p>
               </div>
-              <h2 className="header--lg text--left">2.000 A service was provided but it was not necessary </h2>
+
+              <h2 className="header--md text--left">2.000 A service was provided but it was not necessary </h2>
               <div className="background--gray pad-all text--left">
-                <p>
-                  Info
+                <p className="text--sm">
+                  Medicaid enrollment → <b>Patient onboarding</b> → Diagnosis / treatment → Claims → Post-treatment
+                </p>
+                <div className="columns">
+                  <div className="columns__item--2 container--align-center">
+                    <div className="pad-top text--center text--serif text--xl">10.8%</div>
+                    <p className="text--center">Of Fraud Cases</p>
+                  </div> 
+                  <div className="columns__item--2 container--align-center">
+                    <div className="pad-top text--center text--serif text--xl">$55M - $185M</div>
+                    <p className="text--center">Of US Medicaid</p>
+                  </div>  
+                </div>
+                <p className="text--sm">
+                  Examples <br />
+                  A provider orders an MRI for a sprained ankle.<br />
+                  A patient visits multiple providers in order to gain additional prescriptions for a certain drug.<br />
+                  A provider falsifies symptoms on a patient record to order additional laboratory tests.<br />
                 </p>
               </div>
-              <h2 className="header--lg text--left">3.000 A service was provided but a kickback was involved</h2>
+
+              <h2 className="header--md text--left">3.000 A service was provided but a kickback was involved</h2>
               <div className="background--gray pad-all text--left">
-                <p>
-                  Info
+                <p className="text--sm">
+                  Medicaid enrollment → Patient onboarding → <b>Diagnosis / treatment</b> → Claims → Post-treatment
+                </p>
+                <div className="columns">
+                  <div className="columns__item--2 container--align-center">
+                    <div className="pad-top text--center text--serif text--xl">9.1%</div>
+                    <p className="text--center">Of Fraud Cases</p>
+                  </div> 
+                  <div className="columns__item--2 container--align-center">
+                    <div className="pad-top text--center text--serif text--xl">$46M - $156M</div>
+                    <p className="text--center">Of US Medicaid</p>
+                  </div>  
+                </div>
+                <p className="text--sm">
+                  Examples <br />
+                  A nursing home owner requires another provider, such as an ambulance company, to pay the nursing home owner a portion of the money received for rendering services to patients.<br />
+                  A patient accepts money from a provider and is directed to go to a specific clinic.<br />
+                  Providers are paid additional money to refer Medicaid patients to certain hospitals.<br />
                 </p>
               </div>
-              <h2 className="header--lg text--left">4.000 A service was provided but the actor was not eligible</h2>
+              
+              <h2 className="header--md text--left">4.000 A service was provided but the actor was not eligible</h2>
               <div className="background--gray pad-all text--left">
-                <p>
-                  Info
+                <p className="text--sm">
+                  <b>Medicaid enrollment</b> → Patient onboarding → Diagnosis / treatment → Claims → Post-treatment
+                </p>
+                <div className="columns">
+                  <div className="columns__item--2 container--align-center">
+                    <div className="pad-top text--center text--serif text--xl">7.6%</div>
+                    <p className="text--center">Of Fraud Cases</p>
+                  </div> 
+                  <div className="columns__item--2 container--align-center">
+                    <div className="pad-top text--center text--serif text--xl">$38M - $129M</div>
+                    <p className="text--center">Of US Medicaid</p>
+                  </div>  
+                </div>
+                <p className="text--sm">
+                  Examples <br />
+                  A physician allows a non-physician to impersonate a licensed doctor, who medically treats patients and prescribes drugs, and then bills the Medicaid program.<br />
+                  A patient uses a neighbor’s Medicaid card in order to receive Medicaid services.<br />
+                  False information is used to enroll in Medicaid and then receive Medicaid services.<br />
+
                 </p>
               </div>
-              <h2 className="header--lg text--left">5.000 A service was provided but an overcharge was submitted</h2>
+
+              <h2 className="header--md text--left">5.000 A service was provided but an overcharge was submitted</h2>
               <div className="background--gray pad-all text--left">
-                <p>
-                  Info
+                <p className="text--sm">
+                  Medicaid enrollment → Patient onboarding → <b>Diagnosis / treatment</b> → Claims → Post-treatment
+                </p>
+                <div className="columns">
+                  <div className="columns__item--2 container--align-center">
+                    <div className="pad-top text--center text--serif text--xl">N/A</div>
+                    <p className="text--center">Of Fraud Cases</p>
+                  </div> 
+                  <div className="columns__item--2 container--align-center">
+                    <div className="pad-top text--center text--serif text--xl">N/A</div>
+                    <p className="text--center">Of US Medicaid</p>
+                  </div>  
+                </div>
+                <p className="text--sm">
+                  Examples <br />
+                  A common panel of tests, normally billed using a single code, is split up into individual tests and billed as if they were performed separately. <br />
+                  A provider bills for more than 24 hours of treatment services in a day. <br />
+                  A lower cost drug is provided to a patient after Medicaid was billed for a brand name prescription. <br />
                 </p>
               </div>
-              <h2 className="header--lg text--left">6.000 A service was provided but the payment was denied</h2>
+
+              <h2 className="header--md text--left">6.000 A service was provided but the payment was denied</h2>
               <div className="background--gray pad-all text--left">
-                <p>
-                  Info
+                <p className="text--sm">
+                  Medicaid enrollment → Patient onboarding → Diagnosis / treatment → <b>Claims</b> → Post-treatment
+                </p>
+                <div className="columns">
+                  <div className="columns__item--2 container--align-center">
+                    <div className="pad-top text--center text--serif text--xl">N/A</div>
+                    <p className="text--center">Of Fraud Cases</p>
+                  </div> 
+                  <div className="columns__item--2 container--align-center">
+                    <div className="pad-top text--center text--serif text--xl">N/A</div>
+                    <p className="text--center">Of US Medicaid</p>
+                  </div>  
+                </div>
+                <p className="text--sm">
+                  Examples <br />
+                  A provider submits a valid claim but it is denied by the payer.<br />
+                  The amount owed by the insurer to the healthcare provider under the terms of their contract is undervalued.<br />
+                  The insurer overvalued the cost in paying claims.<br />
                 </p>
               </div>
-              <h2 className="header--lg text--left">7.000 A service was provided but it was not covered</h2>
+
+              <h2 className="header--md text--left">7.000 A service was provided but it was not covered</h2>
               <div className="background--gray pad-all text--left">
-                <p>
-                  Info
+                <p className="text--sm">
+                  Medicaid enrollment → Patient onboarding → Diagnosis / treatment → <b>Claims</b> → Post-treatment
+                </p>
+                <div className="columns">
+                  <div className="columns__item--2 container--align-center">
+                    <div className="pad-top text--center text--serif text--xl">N/A</div>
+                    <p className="text--center">Of Fraud Cases</p>
+                  </div> 
+                  <div className="columns__item--2 container--align-center">
+                    <div className="pad-top text--center text--serif text--xl">N/A</div>
+                    <p className="text--center">Of US Medicaid</p>
+                  </div>  
+                </div>
+                <p className="text--sm">
+                  Examples <br />
+                  An over the counter drug that is not covered by Medicaid is nevertheless prescribed and billed to Medicaid.<br />
+                  A patient uses an ambulance provided by Medicaid for personal use (versus medical use).<br />
+                  A patient receives an uncovered cosmetic dental procedure yet the provider bills it as a similar but covered Medicaid procedure.<br />
+
                 </p>
               </div>
               <br />
@@ -235,30 +356,30 @@ class FraudWasteAbuseHealthcare extends Component {
               
               <div className="columns">
                 <div className="columns__item--4 container--align-center">
-                  <div className="background--gray pad-all text--center text--serif text--lg"> $1.1B </div>
+                  <div className="background--gray pad-all text--center text--serif text--xl"> $1.1B </div>
                   <p className="text--center">Dollars Invested In FWA Recovery</p>
                 </div>       
                 <div className="columns__item--4 container--align-center">
-                  <div className="background--gray pad-all text--center text--serif text--lg"> $1.9B </div>
+                  <div className="background--gray pad-all text--center text--serif text--xl"> $1.9B </div>
                   <p className="text--center">Dollars <br /> Recovered</p>
                 </div>
                 <div className="columns__item--4 container--align-center">
-                  <div className="background--gray pad-all text--center text--serif text--lg"> 0.15% </div>
+                  <div className="background--gray pad-all text--center text--serif text--xl"> 0.15% </div>
                   <p className="text--center">Percent of loss Recovered</p>
                 </div> 
                 <div className="columns__item--4 container--align-center">
-                  <div className="background--gray pad-all text--center text--serif text--lg">0.026%</div>
+                  <div className="background--gray pad-all text--center text--serif text--xl">0.026%</div>
                   <p className="text--center">Percent of Spending Put Towards Recovery</p>
                 </div>
               </div>
 
               <p>
-              While FY2021 was on the lower end of success during the three year rolling average, the HCFAC
-              made $0.9B from their efforts. Only 0.15% of loss to fraud, waste, and abuse was recovered, 
-              leaving tons of money intended for healthcare use unrecovered. 
+                While FY2021 was on the lower end of success during the three year rolling average, the HCFAC
+                made $0.9B from their efforts. Only 0.15% of loss to fraud, waste, and abuse was recovered, 
+                leaving tons of money intended for healthcare use unrecovered. 
 
-              The $0.9B success, paired with the low 0.026% of US Healthcare spending allocated to the 
-              HCFAC for recovery efforts, makes us question why not invest more? 
+                The $0.9B success, paired with the low 0.026% of US Healthcare spending allocated to the 
+                HCFAC for recovery efforts, makes us question why not invest more? 
               </p>
 
               <Divider />

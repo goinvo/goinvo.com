7e2e27e28e0c09821b41ffcd1a656b67e003a1fb
EricBenoit <benwa02@gmail.com>
2024-08-09 17:57:48 -0400

    Revising content, improving css styling.

--- src/pages/enterprise.js
+++ src/pages/enterprise.js
@@ -4,10 +4,8 @@ import Layout from '../components/layouts/layout'
 import config from '../../config'
 import Image from '../components/image'
 import ClientLogos from '../components/client-logos'
-// import Columns from '../components/columns'
-// import ImageBlock from '../components/image-block'
 import Card from '../components/card'
-// import Quote from '../components/quote'
+import HubspotForm from '../components/hubspot-form'
 
 const frontmatter = {
   metaTitle: 'Design for Enterprise Software',
@@ -25,11 +23,12 @@ class EnterprisePage extends Component {
             <div className="max-width content-padding pad-vertical LayoutCenter">
               <div className="flip-parent container-spacing">
                 <div className="flip-child container-spacing">
-                  <h1 className="header--xl ">
-                    Results by Design
+                  <h1 className="header--xl margin-bottom--none">
+                    Enterprise is complicated<span className="text--serif text--primary">.</span><br />
+                    We know how to do it
                     <span className="text--serif text--primary">.</span>
                   </h1>
-                  <p>
+                  <p className="margin-top--none">
                     Beautiful software design for the Enterprise to catapult your business forward.
                   </p>
                   <Link
@@ -52,14 +51,14 @@ class EnterprisePage extends Component {
           </div>
           <div className="bg-light pad-horizontal">
             <div className="max-width content-padding pad-vertical LayoutCenter teal">
-            <h2 className="header--xl headerStyle ">
+              <h2 className="header--xl">
                 GoInvo enables high-growth companies to realize market-changing advances via envisioning, planning, and implementing best-in-class enterprise software, services, and experiences.
-                </h2>
-                <p>
-                  What results are you looking for?
-                </p>
+              </h2>
+              <p className="margin-bottom--none">
+                What results are you looking for?
+              </p>
               <div className="pure-u-lg-1-2">
-                <h4>
+                <h4 className="margin-top--none">
                   Cost savings through a more productive workforce or customers.
                 </h4>
                 <h4>
@@ -70,7 +69,7 @@ class EnterprisePage extends Component {
                 </h4>
               </div>
               <div className="pure-u-lg-1-2">
-                <h4>
+                <h4 className="margin-top--none">
                   A vision to rally the company.
                 </h4>
                 <h4>
@@ -90,57 +89,59 @@ class EnterprisePage extends Component {
               </h2>
             </div>
 
-            <div className="pad-horizontal pure-u-sm-1-3">
-              <Image
-                src="/images/open_source/innovation.png"
-                className="image--max-width-80"
-                sizes={config.sizes.fullToHalfAtLargeInsideMaxWidth}
-              />
-              <p className="text">
-                <strong>Software Design Velocity</strong><br />
-                Start fast with a team experienced working together compared to hiring.
-              </p>
-              <p>
-                <span className="nbrStyle">100+</span>
-                <span className="center">
-                  software design projects since 2010
-                </span>
-              </p>
-            </div>
-            <div className="pad-horizontal pure-u-sm-1-3">
-              <Image
-                src="/images/open_source/public-good.png"
-                className="image--max-width-80"
-                sizes={config.sizes.fullToHalfAtLargeInsideMaxWidth}
-              />
-              <p className="text">
-                <strong>Large-Scale Enterprise Design</strong><br />
-                Make design and innovation a key influence in your organization.
-              </p>
-              <p>
-                <span className="nbrStyle">160M+</span>
-                <span className="center">
-                  People impacted by GoInvo designs
-                </span>
-              </p>
-            </div>
+          <div className="container--justify-center">
+              <div className="pad-horizontal pure-u-sm-1-3">
+                <Image
+                  src="/images/open_source/innovation.png"
+                  className="image--max-width-80"
+                  sizes={config.sizes.fullToHalfAtLargeInsideMaxWidth}
+                />
+                <p className="text margin-top--none">
+                  <strong>Software Design Velocity</strong><br />
+                  Start fast with a team experienced working together compared to hiring.
+                </p>
+                <p>
+                  <span className="nbrStyle">100+</span>
+                  <span>
+                    software design projects since 2010
+                  </span>
+                </p>
+              </div>
+              <div className="pad-horizontal pure-u-sm-1-3">
+                <Image
+                  src="/images/open_source/public-good.png"
+                  className="image--max-width-80"
+                  sizes={config.sizes.fullToHalfAtLargeInsideMaxWidth}
+                />
+                <p className="text margin-top--none">
+                  <strong>Large-Scale Enterprise Design</strong><br />
+                  Make design and innovation a key influence in your organization.
+                </p>
+                <p>
+                  <span className="nbrStyle">160M+</span>
+                  <span>
+                    people impacted by GoInvo designs
+                  </span>
+                </p>
+              </div>
 
-            <div className="pad-horizontal pure-u-sm-1-3">
-              <Image
-                src="/images/open_source/trust.png"
-                className="image--max-width-80"
-                sizes={config.sizes.fullToHalfAtLargeInsideMaxWidth}
-              />
-              <p className="text">
-                <strong>Feature Finding for Profits</strong><br />
-                We discover opportunities that keep clients coming back for more.
-              </p>
-              <p>
-                <span className="nbrStyle">90%</span>
-                <span className="center">
-                  of clients repeat business with GoInvo
-                </span>
-              </p>
+              <div className="pad-horizontal pure-u-sm-1-3">
+                <Image
+                  src="/images/open_source/trust.png"
+                  className="image--max-width-80"
+                  sizes={config.sizes.fullToHalfAtLargeInsideMaxWidth}
+                />
+                <p className="text margin-top--none">
+                  <strong>Feature Finding for Profits</strong><br />
+                  We discover opportunities that keep clients coming back for more.
+                </p>
+                <p>
+                  <span className="nbrStyle">90%</span>
+                  <span>
+                    of clients repeat business with GoInvo
+                  </span>
+                </p>
+              </div>
             </div>
           </div>
 
@@ -161,13 +162,13 @@ class EnterprisePage extends Component {
             <div className="max-width content-padding pad-vertical container--justify-center center ">
               <div className="paddingBottom">
                 <div>
-                  <h2 className="header--xl">
+                  <h2 className="header--xl margin-bottom--none">
                     Driving results for Enterprise software
                     <span className="text--serif text--primary">.</span>
                   </h2>
                   <Link
                     to="/contact"
-                    className="button button--primary button--lg ctaLayout"
+                    className="button button--primary button--lg ctaLayout margin-bottom"
                   >
                     Let's discuss your project
                   </Link>
@@ -177,22 +178,23 @@ class EnterprisePage extends Component {
                   <Card link="/work/3m-coderyte/">
                     <div className="flip-parent">
                       <div className="pure-u-lg-1-2 teal textCard flip-child">
-                        <h4 className="header--xl headerCard">
-                          A 200% productivity gain = $146M exit.
+                        <h4 className="header--xl headerCard margin-bottom--none">
+                          A 200% productivity gain <br />
+                          = $146M exit.
                         </h4>
-                        <p className="text">
+                        <p className="header--lg margin-bottom--none">
+                          “We needed to make sure we could do this. Invo proved we could. We needed to make sure we could sell this. Invo gave us the tools to do that, too.”
+                        </p>
+                        <p className="margin-top--none">
+                          George Moon<br />
+                          VP of Product, CodeRyte Inc.
+                        </p>
+                        <p>
                           <Link
                             to="/work/3m-coderyte/"
                           >
                             Read Case Study
                           </Link>
-                        </p>
-                        <p>
-                          “We needed to make sure we could do this. Invo proved we could. We needed to make sure we could sell this. Invo gave us the tools to do that, too.”
-                        </p>
-                        <p>
-                          George Moon<br />
-                          VP of Product, CodeRyte Inc.
                         </p>
                       </div>
 
@@ -214,19 +216,19 @@ class EnterprisePage extends Component {
                         <h4 className="header--xl headerCard">
                           A vision to secure $17M Series B.
                         </h4>
-                        <p className="text">
+                        <p className="header--lg margin-bottom--none">
+                          “We absolutely love everything GoInvo has created for us, it has by far exceeded our expectations.”
+                        </p>
+                        <p className="margin-top--none">
+                          Serban Georgescu MD<br />
+                          Director of Business and Clinical Development, InfoBionic
+                        </p>
+                        <p>
                           <Link
                             to="/work/infobionic-heart-monitoring/"
                           >
                             Read Case Study
                           </Link>
-                        </p>
-                        <p>
-                          “We absolutely love everything GoInvo has created for us, it has by far exceeded our expectations.”
-                        </p>
-                        <p>
-                          Serban Georgescu MD<br />
-                          Director of Business and Clinical Development, InfoBionic
                         </p>
                       </div>
 
@@ -247,27 +249,26 @@ class EnterprisePage extends Component {
           <div className="bg-light teal">
             <div className="max-width content-padding pad-vertical--double">
               <div className="pure-u-md-1-2">
-                <h2 className="header--xl">
+                <p className="header--xl">
+                  <em>Enterprise is complicated.<br />
+                  Your stuff looks like shit.<br />
+                  Nobody wants to use it.<br />
+                  We ship software that works.</em>
+                </p>
+              </div>
+
+              <div className="pure-u-md-1-2">
+              <h2 className="header--xl margin-bottom--none">
                   Let's build together!
                 </h2>
-                <p>
+                <p className="margin-top--none">
                   Interested in having GoInvo help your organization?<br />
                   Reach out to learn more about our services and how we can help!
                 </p>
 
-                <Link
-                  to="/contact"
-                  className="button button--primary button--lg ctaLayout"
-                >
-                  Let's discuss your project
-                </Link>
-              </div>
-
-              <div className="pure-u-md-1-2 hidden--sm">
-                <Image
-                  src="/images/open_source/cta-community.png"
-                  className="image--max-width"
-                  sizes={config.sizes.full}
+                <HubspotForm
+                  formId={config.hubspotContactFormId}
+                  title=""
                 />
               </div>
             </div>

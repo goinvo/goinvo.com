cbe5d7f67544b335ad7c04942b8fa16028d88e19
Shirley Xu <Mirative@gmail.com>
2025-09-18 14:28:50 -0400

    Removed gradient on 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,7 +6,7 @@ import config from '../../config'
 const NotFoundPage = () => (
   <Layout>
     <div className="max-width content-padding pad-vertical" style={{ textAlign: 'center', paddingBottom: 0, marginBottom: 0 }}>
-      <h1 className="header--xl" style={{ fontWeight: 700 }}>NOT FOUND</h1>
+      <h1 className="header--xl" style={{ fontWeight: 700 }}>NOT FOUND...</h1>
       <p style={{ fontSize: '1.125rem', marginBottom: '12px' }}>You just hit a route that doesn't exist... the sadness.</p>
       <img
         src={`${config.cloudfrontUrl}/images/404/404-website.png`}
@@ -18,9 +18,7 @@ const NotFoundPage = () => (
           height: 'auto',
           display: 'block',
           margin: '12px auto 0',
-          clipPath: 'inset(24px 3px 0 0)',
-          WebkitMaskImage: 'linear-gradient(to bottom, black 93%, transparent)',
-          maskImage: 'linear-gradient(to bottom, black 93%, transparent)'
+          clipPath: 'inset(24px 3px 0 0)'
         }}
       />
     </div>
